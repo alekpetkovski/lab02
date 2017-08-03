53e84f35c3ef62207b5a88de2339be899af126fb
alek <z5164896@unsw.edu.au>
2017-08-03 23:19:35 +1000

    added funtion that indicates whether it is a fizz, buzz or fizzbuzz from 1-100

---
+++ fizzbuzz-untill-100.py
@@ -0,0 +1,15 @@
+def function():
+	for n in range(1,100):
+		if n % 3 == 0 and n % 5 == 0:
+			print ('Fizzbuzz')
+
+		elif n % 3 == 0:
+			print ('Fizz')
+
+		elif n % 5 == 0:
+			print ('Buzz')
+
+		else:
+			print (n)
+
+function()
